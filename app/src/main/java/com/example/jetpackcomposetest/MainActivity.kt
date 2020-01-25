15b38ae6cc67b4d6e547e9afe7e458d327517eea
Yuria Endo <churiping@gmal.com>
2020-01-25 20:17:21 +0900

    importしたよー！

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -7,10 +7,14 @@ import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+    lateinit var geofencingClient: GeofencingClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        geofencingClient = LocationServices.getGeofencingClient(this)
         setContent {
             MaterialTheme {
                 Greeting("Yuria")
